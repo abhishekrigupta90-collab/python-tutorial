ba873fdad9497993245895d849bd581f04f6345a
abhishekrigupta90-collab <abhishekrigupta90@gmail.com>
2026-07-12 22:56:05 +0545

    first commit

---
+++ modules.py
@@ -0,0 +1,21 @@
+# print(float(10))
+# print(int(9.4))
+# hat_height = 10
+# man_height =40
+# # find total height of man when he use hat
+# total_height = hat_height + man_height 
+
+# print(total_height)
+# print(int('10'))
+
+
+# a = int(input("enter a number  a :"))
+# b = int(input("enter a number b:"))
+# print("number is a",a)
+# print("number is B",b)
+
+# print("sum of two number", a / b)
+
+a = int(input("enter a number 1 :"))
+b = int(input("enter a number 2 :"))
+print("a is greater than b or not:" , (a+b)/2) 
